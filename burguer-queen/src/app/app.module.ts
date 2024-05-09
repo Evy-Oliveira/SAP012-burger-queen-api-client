584d8aee7a68bbe51946865c43a5b599bf69009c
Evy-Oliveira <evelyn.o-s@hotmail.com>
2024-05-09 18:07:40 -0300

    Inclusão da estrutura inicial + changelog

--- burguer-queen/src/app/app.module.ts
+++ burguer-queen/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { TelaLoginComponent } from './components/views/tela-login/tela-login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderLoginComponent } from './components/headers/header-login/header-login.component';
+import { FormLoginComponent } from './components/form-login/form-login.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TelaLoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeaderLoginComponent,
+    FormLoginComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
