b7995f90604d1cd417a5f91277ef4b6328c69007
Evy-Oliveira <evelyn.o-s@hotmail.com>
2024-05-13 19:58:35 -0300

    inclusão -  componentes header, form-login e footer no componente page-login. Correções na instalação do angular material

--- burguer-queen/src/app/app.module.ts
+++ burguer-queen/src/app/app.module.ts
@@ -1,28 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderLoginComponent } from './components/headers/header-login/header-login.component';
-import { FormLoginComponent } from './components/form-login/form-login.component';
 import { ButtonComponent } from './components/button/button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { FormLoginComponent } from "./components/form-login/form-login.component";
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    FooterComponent,
-    HeaderLoginComponent,
-    FormLoginComponent,
-    ButtonComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        FooterComponent,
+        HeaderLoginComponent,
+        ButtonComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        FormLoginComponent,
+        MatButtonModule
+    ]
 })
 export class AppModule { }
--- burguer-queen/src/app/components/form-login/form-login.component.ts
+++ burguer-queen/src/app/components/form-login/form-login.component.ts
@@ -1,10 +1,27 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-form-login',
   templateUrl: './form-login.component.html',
-  styleUrls: ['./form-login.component.css']
+  styleUrls: ['./form-login.component.css'],
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, FormsModule, ReactiveFormsModule, NgIf]
 })
 export class FormLoginComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+hide = true;
+
+  getErrorMessage(){
+    if(this.email.hasError('required')){
+      return 'Você deve inserir um e-mail válido'
+    }
+    return this.email.hasError('email') ? 'E-mail inválido' : '';
+  }
 
 }
